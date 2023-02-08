75c348dab32cc7bf0e9f7f8f8fb326e60a778c2f
Anthony Lee <antleee7@snu.ac.kr>
2023-02-09 03:47:53 +0900

    fully implemented structure of FFBD

--- harang/AIR.py
+++ harang/AIR.py
@@ -11,6 +11,7 @@ import numpy as np
 from math import sqrt
 from lib.stateuplink import State
 # from lib.statedownlink import State --> 여기에 telemetry data패킷 형식도 포함해야 함
+# from lib.kalman import Kalman --> 칼만 라이브러리 만들어야 함.
 from lib.comm import Comm
 from lib.sensor import Sensor
 from lib.gpio import Pin
@@ -22,10 +23,16 @@ def rad2deg(rad):
 if __name__ == "__main__":
 
 
-    launchready      = False # Launch Ready Mode에 진입하라는 명령을 받으면 True가 됩니다.
+    launchready      = False # Launch Ready Mode에 진입하라는 명령을 받으면 True가 됩니다. Launch detection 이후에는 False가 됩니다.
     supervised       = False # 강제사출 명령을 받으면 True가 됩니다.
     
     launched         = False # Launch detection이후 True가 됩니다.
+    
+    apogee           = False # Apogee 도달 이후 True가 됩니다.
+    
+    deploy           = False # PAYLOAD의 drogue deploy를 담당합니다.
+    
+    touchdown        = False # 로켓이 착륙했는지 판단합니다.
     
     emergency        = False # 각속도가 20dps가 넘는 비상상황시 True가 됩니다.
 
@@ -93,7 +100,7 @@ if __name__ == "__main__":
         new_state = radio.read(); # 지상국의 명령을 받습니다.
         
         # Housekeeping data downlink (Low Data Rate)
-        radio.write(data) SHIT # write그대로 쓰면 됨. 다만 어떤식으로 보낼지는 모르겠음.
+        radio.write(HOUSEKEEPING_DATA) SHIT # write그대로 쓰면 됨. 다만 어떤식으로 보낼지는 모르겠음.
         
         # Battery Voltage and Temperature Check
         voltage = sensor.voltage()
@@ -119,19 +126,61 @@ if __name__ == "__main__":
             # Get bit flags from new state
             launchready  = state.get_bit(state.LAUNCH_BIT, byte=new_state)
             supervised   = state.get_bit(state.DEPLOY_BIT, byte=new_state)
+            
 
-            if launchready:
-                PROCEED TO LAUNCH READY MODE
-            if supervised:
-                DEPLOY DROGUE CHUTE
 
         #######################################################################
         ## Do repeated actions (i.e. read from sensors) depending on latches
         #######################################################################
         
-        # Emergency Recovery Mode
-        if ANGULAR VELOCITY > 20DPS:
+        if launchready: # Launch Ready mode가 시작됩니다.
+            radio.maxDataRate...? # 한번만 실행되면 되는데, 굳이 while문 안에 넣을 필요...?
+            radio.write(TELEMETRY_DATA) # 지상국으로 Telemetry data를 보냅니다. (종료될때까지 유지되어야 하는데, 어떻게 구현하지? --> 반복적으로 실행되어야 하는 것들을 정리해놓고, 각각에 해당하는 상태들을 정의하면 될듯. 예를 들어 여기서 직접 telemetry data를 보내는 게 아니라 telemetry_write = True같이 해놓는거지. while문 안에 또 다른 if telemetry_write: 이런게 있는것이고. if 문을 병렬적으로 씁시다.)
+            _logging_on = True # 마찬가지로 한번만 실행되면 됨... 'Telemetry Data Memory Writing Start'
+            SUBSYSTEM_BAUD_RATE_INCREASE
+            if sensor.imu_data["accel"] > 1.5:
+                launched = True # Launch Recognition
+                launchready = False # Launch Ready Mode를 종료합니다.
+                
+        if launched: # In-flight mode가 시작됩니다.
+            ORIENTATION CHECK VIA IMU (KALMAN)
+            if ALTITUDE < MAX_ALTITUDE - 1.0:
+                apogee = True
+            if ANGULAR_VELOCITY < 20DPS and (apogee or supervised) and FLIGHT_TIME > 20s:
+                deploy = True # Drogue를 사출합니다.
+                launched = False # In-flight mode를 종료합니다.
+                
+        if deploy: # Recovery mode가 시작됩니다.
+            PAYLOAD DROGUE DEPLOY
+            if PROXIMITY_SENSOR reads 'some threshold':
+                if ORIENTATION_STABILIZED:
+                    ROCKET DROGUE DEPLOY # TODO: 한 번만 실행되게끔.
+            if GPS, INS READ TOUCHDOWN:
+                touchdown = True
+                deploy = False # Recovery mode가 종료됩니다.
+                
+        if touchdown:
+            ALL SUBSYSTEMS DOWN EXCEPT COMM SYSTEM
+            HOUSEKEEPING DATA DOWNLINK (MIN DATARATE)
+                
+                
+###########DOWNLINK DATA를 병렬 if문으로 구현해야겠음.
+            
+        
+        # 각속도 감시
+        if ANGULAR_VELOCITY > 20DPS:
             emergency = True # 전역변수 안써도 되나??
+            
+            
+        if emergency: # 비상상황
+            ALERT DATA DOWNLINK AND DEPLOYMENT COMMAND MONITOR
+            APOGEE MONITOR VIA BAROMETER
+            APOGEE MONITOR VIA GPS
+            
+            if or or :
+                deploy = True
+                emergency = False # 비상상황 종료
+            
         
         # If logging is on, write IMU data to logfile! TODO: We have yet to 
         # implement sensor logging from the BMP280 because its read speed is 
